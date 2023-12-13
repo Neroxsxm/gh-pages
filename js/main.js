13fef9d7e1600ca919eaf056151eccb17f254c25
Sean <nerox.sxm@gmail.com>
2023-12-13 11:50:31 +0100

    page panier

--- js/main.js
+++ js/main.js
@@ -8,8 +8,32 @@ function ajouterAuPanier() {
 
 // Fonction pour ouvrir le panneau (à personnaliser selon vos besoins)
 function ouvrirPanneau() {
-    alert("Ouvrir le panneau ici !");
+    window.location.href="./panier.html";
 
     // Vous pouvez ajouter ici la logique pour afficher le panneau, le panier, etc.
     // Par exemple, en manipulant les classes CSS, en modifiant le DOM, ou en utilisant une bibliothèque JavaScript.
+}
+
+// Fonction pour retirer un produit du panier
+function retirerDuPanier() {
+    // Supprimez le dernier article du panier (à adapter en fonction de votre implémentation réelle)
+    var panier = document.querySelector(".cart");
+    var articles = panier.querySelectorAll(".cart-item");
+
+    if (articles.length > 0) {
+        articles[articles.length - 1].remove();
+    } else {
+        alert("Le panier est vide.");
+    }
+}
+
+// Fonction pour passer la commande
+function passerLaCommande() {
+    // Logique de passer la commande ici
+    alert("Commande passée ! Merci pour votre achat.");
+
+    // Vous pouvez ajouter ici la logique pour vider le panier, envoyer la commande au serveur, etc.
+    // Par exemple, en réinitialisant la liste des articles dans le panier.
+    var panier = document.querySelector(".cart");
+    panier.innerHTML = "<h2>Panier</h2><p>Le panier est vide.</p>";
 }
