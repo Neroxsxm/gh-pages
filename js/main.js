19b95814319118ab4a7896b9317b65d50184a297
Sean <nerox.sxm@gmail.com>
2023-12-13 11:24:49 +0100

    panier cliquable

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Fonction pour ajouter un produit au panier
+function ajouterAuPanier() {
+    alert("Produit ajouté au panier !");
+}
+
+// Vous pourriez ajouter ici d'autres fonctionnalités JavaScript liées à votre site de e-commerce
+// Par exemple, la gestion du panier, des requêtes AJAX pour récupérer des données du serveur, etc.
+
+// Fonction pour ouvrir le panneau (à personnaliser selon vos besoins)
+function ouvrirPanneau() {
+    alert("Ouvrir le panneau ici !");
+
+    // Vous pouvez ajouter ici la logique pour afficher le panneau, le panier, etc.
+    // Par exemple, en manipulant les classes CSS, en modifiant le DOM, ou en utilisant une bibliothèque JavaScript.
+}
